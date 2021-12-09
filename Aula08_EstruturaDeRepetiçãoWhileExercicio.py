3a58ea0df6fdf4f95828c9adb1c8ad73e8fd5bce
Ester Cardoso Toja <87045785+estercardosotoja@users.noreply.github.com>
2021-12-09 15:50:34 -0300

    Aulas 03 a 08 Paython (#1)
    -  Tipo Primitivos 
    - Operações Matemáticas 
    - Estruturas Condicionais
    - Estruturas Lógicas
    - Estruturas de Repetição
                   - for 
                   - while

---
+++ Aula08_EstruturaDeRepetiçãoWhileExercicio.py
@@ -0,0 +1,8 @@
+"""
+
+1) Faça um programa que calcule o maior palindromo resultado do produto de dois números de 3 digitos.
+Polindromo são numeros que lendo da esquerda para a direita resulta no mesmo caso leia da direita para a esquerda.
+EX: 52625
+EX: O maior políndromo resultado do produto de dois números é 91*99 = 9009
+
+"""
