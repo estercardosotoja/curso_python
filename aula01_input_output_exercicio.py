1072cd231e0cd902e41e5bf140aac7050c24befc
Ester Cardoso Toja <87045785+estercardosotoja@users.noreply.github.com>
2021-12-09 16:02:28 -0300

    Add files via upload
    Aulas Iniciais do Curso de Python
    - PEP
    - Entrada e Saída
    - Ajuda Dir e Help
    - Inicio de Tipo de Dados

---
+++ aula01_input_output_exercicio.py
@@ -0,0 +1,14 @@
+"""
+1 - Imprimir na tela a mensagem: "Olá Mundo"
+"""
+
+print('Olá Mundo');
+
+"""
+2 - Receber nome e idade do usuário e imprimi-los em um unico print.
+"""
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print(f'Seu nome é: {nome} Idade: {idade}');
