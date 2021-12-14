c253d7b7c6c90b4e2946964293c76e19ee1d486e
Ester Cardoso Toja <87045785+estercardosotoja@users.noreply.github.com>
2021-12-14 11:34:26 -0300

    Aulas 10 e 11 (#4)

--- udemy/Aula07_EstruturasDeRepeticao_Exercicio.py
+++ udemy/Aula07_EstruturasDeRepeticao_Exercicio.py
@@ -1,7 +1,7 @@
 """
 1) Faça um programa que calcule a soma de divisores de um um número inteiro definindo pelo usuário.
 EX: Se o usuário escolheu 10, temos =  1 + 2 + 5 + 10 = 18
-
+"""
 
 soma = 0
 num = int(input('Digite um numero: '))
@@ -10,7 +10,6 @@ for numero in range(1, num + 1):
    if num % numero == 0:
         soma = soma + numero
 print(soma)
-"""
 
 """
 2) Faça um programa que imprima os n primeiros multplos  de 5, sendo n definido pelo usuario .
