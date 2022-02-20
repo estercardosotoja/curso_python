aed8bc473a3d43a2c4a6925c8e672b9e992b5e12
estercardosotoja <ester.toja15@gmail.com>
2022-02-19 22:43:04 -0300

    Termino do Exercicio

--- udemy/Aula19_Funcoes_sem_parametrosExercicios.py
+++ udemy/Aula19_Funcoes_sem_parametrosExercicios.py
@@ -1,12 +1,11 @@
 """
-1) Foi realizada uma pesquisa de algumas características e gostos de quatro
-habitantes incluindo: nome, sexo, esporte favorito (Natação, Futebol, Volêi,
-Tênis) e idade. Com esses dados faça:
-- Função que armazene os dados em uma lista. Dica: Use dicionários dentro da
-lista.
-- Calcule a idade média de homens que gostam de natação. Caso não haja
-homens que gostam de natação chame uma função e imprima um aviso de que
-não há homens que gostam de natação0
+
+    1) Foi realizada uma pesquisa de algumas características e gostos de quatro habitantes incluindo:
+    nome, sexo, esporte favorito (Natação, Futebol, Volêi, Tênis) e idade. Com esses dados faça:
+    - Função que armazene os dados em uma lista. Dica: Use dicionários dentro da lista.
+    - Calcule a idade média de homens que gostam de natação. Caso não haja homens que gostam
+    de natação chame uma função e imprima um aviso de que não há homens que gostam de natação
+
 """
 
 def questionario():
@@ -18,15 +17,15 @@ def questionario():
     return dados
 
 def escolha_esporte():
-    opcao = input("Digite o codigo refente ao esporte \n 1 - Natação, \n 2 - Futebol, \n 3 - Volêi, \n 4 - Tênis ")
+    opcao = (input("Digite o codigo refente ao esporte \n 1 - Natação, \n 2 - Futebol, \n 3 - Volêi, \n 4 - Tênis \n Opção: "))
     if opcao == "1":
-        return "Natação"
+        return "natacao"
     elif opcao == "2":
-        return  "Futebol"
+        return  "futebol"
     elif opcao == "3":
-        return "Volei"
+        return "volei"
     elif opcao == "4":
-        return "Tenis"
+        return "tenis"
     else:
         print('OPCAO INVALIDA')
         return
@@ -34,29 +33,33 @@ def escolha_esporte():
 def inicio():
     sair = ('')
     habitantes.append(questionario())
-    sair = input(" Adicionar mais alguém? s/n")
+    sair = input("Adicionar mais alguém? s/n")
     if sair != "n":
         inicio()
     else:
         print(f"Fim \n Habitantes {habitantes}")
 
 def aviso():
-    print("Não há homens que gostam de natação")
+
+    return "Não há homens que gostam de natação"
+
+def respostas():
+    total = len(habitantes)
+    print(total)
+    i = 0
+    soma = 0
+    homens_natacao = 0
+    for i in range(total):
+        if habitantes[i]['sexo'] == 'm' and habitantes[i]['esporte'] == 'natacao':
+            soma = soma + habitantes[i]['idade']
+            homens_natacao += 1
+
+        if homens_natacao == 0:
+            return aviso()
+        else:
+            return f"A idade média de homens que fazem natação é: {soma /homens_natacao}"
 
 habitantes = []
-soma = 0
 inicio()
-cont = 0
-print(habitantes.get('sexo'))
-for habitante in habitantes:
-    if habitante.get('sexo') in habitante:
-        if habitante.get('natacao') in habitante:
-           soma =+ habitante.get("idade")
-           cont = cont + cont;
-    else:
-        aviso()
-
-print("Fim")
-
-
+print(respostas())
 
